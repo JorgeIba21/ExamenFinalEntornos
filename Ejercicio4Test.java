f2dce395e6c50c08a2072e7681acc92681fa3bc7
Jorge Ibáñez <59049245+JorgeIba21@users.noreply.github.com>
2020-05-25 10:28:30 +0200

    Segundo commit JUnit

--- Ejercicio4Test.java
+++ Ejercicio4Test.java
@@ -14,6 +14,21 @@ class Ejercicio4Test {
 		assertEquals("c", Ejercicio4.ipAdressType("200.30.110.5", null));
 		assertEquals("c", Ejercicio4.ipAdressType("200.30.110.5", ""));
 		assertEquals("c", Ejercicio4.ipAdressType("200.30.110.5", "255.255.255.0"));
+		assertTrue(Ejercicio4.ipAdressType("200.30.110.5/24", "") == "c");
+	}
+	
+	@Test
+	void testGetArea() {
+		assertEquals(10.825317547305483, Ejercicio4.getArea(5, 5, 5));
+		assertEquals(84, Ejercicio4.getArea(13, 15, 14));
+		
+	}
+	
+	@Test
+	void testTriangle() {
+		assertEquals("Triangular Taper", Ejercicio4.triangle(5, 6, 7));
+		assertEquals("Right Triangle", Ejercicio4.triangle(3, 4, 5));
+		assertEquals("Blunt Triangle", Ejercicio4.triangle(3, 4, 6));
 	}
 
 }
