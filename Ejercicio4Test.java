638654cb9a86a035c093c8b65baada8f440f30c3
Jorge Ibáñez <59049245+JorgeIba21@users.noreply.github.com>
2020-05-25 10:35:27 +0200

    Último commit JUnit

--- Ejercicio4Test.java
+++ Ejercicio4Test.java
@@ -21,6 +21,18 @@ class Ejercicio4Test {
 	void testGetArea() {
 		assertEquals(10.825317547305483, Ejercicio4.getArea(5, 5, 5));
 		assertEquals(84, Ejercicio4.getArea(13, 15, 14));
+		assertThrows(RuntimeException.class, () -> {
+		      Ejercicio4.getArea(2, 2, 4);
+		    });
+		assertThrows(RuntimeException.class, () -> {
+			Ejercicio4.getArea(-5, -2, -4);
+		});
+		assertThrows(RuntimeException.class, () -> {
+			Ejercicio4.getArea(7, 2, 4);
+		});
+		assertThrows(RuntimeException.class, () -> {
+			Ejercicio4.getArea(2, 0, 4);
+		});
 		
 	}
 	
@@ -29,6 +41,16 @@ class Ejercicio4Test {
 		assertEquals("Triangular Taper", Ejercicio4.triangle(5, 6, 7));
 		assertEquals("Right Triangle", Ejercicio4.triangle(3, 4, 5));
 		assertEquals("Blunt Triangle", Ejercicio4.triangle(3, 4, 6));
+		assertThrows(RuntimeException.class, () -> {
+			Ejercicio4.triangle(2, 2, 2);
+		});
+		assertThrows(RuntimeException.class, () -> {
+			Ejercicio4.triangle(-2, 5, 2);
+		});
+		assertThrows(RuntimeException.class, () -> {
+			Ejercicio4.triangle(2, 0, 8);
+		});
+		
 	}
 
 }
